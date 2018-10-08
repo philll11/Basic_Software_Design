db23037dadd6a18c52873cf8a1a948d8ac38d4dd
Leonard <leonardjohnphillips@gmail.com>
2018-10-09 11:41:40 +1300

    first commit

---
+++ src/main/java/MemAppender.java
@@ -0,0 +1,5 @@
+public class MemAppender {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/VelocityLayout.java
@@ -0,0 +1,5 @@
+public class VelocityLayout {
+    public static void main(String[] args) {
+
+    }
+}
